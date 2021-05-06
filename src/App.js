0a6e69a45db8b6fddbbdd8b69c0f9e233fa214fa
diego <d.garciabrisa@gmail.com>
2021-05-06 17:17:57 +0200

    adding landing and landing img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Landing from "./components/Landing/Landing";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
+          <Route path={'/'} exact component={Landing} />
           
         </Switch>
       </BrowserRouter>
---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import Login from '../Login/Login'
+import Register from '../Register/Register'
+
+const Landing = (props) => {
+
+    const [active, setActive] = useState(true)
+
+    
+    const register = () => {
+        setTimeout(()=>{
+            setActive(!active)
+        },500)
+    }
+
+
+
+    return (
+        <div className="landingsComponent">
+            <div className="landingContainers landingLeft"></div>
+            <div className="landingContainers landingRight">
+                <div className="spacer"></div>
+                <div className="spacer"></div>
+                <div className="spacer"></div>
+                {
+                    active ? 
+                    <>
+                        <Login/> 
+                        <div className="registerLanding">
+                            <p>Don't have an account?</p>
+                            <p className="register" onClick={()=>register()}>Register</p>
+                        </div>
+                    </>
+
+                    : 
+                    <>
+                        <Register />
+                        <div className="registerLanding">
+                            <p>Do you have an account?</p>
+                            <p className="register" onClick={()=>register()}>Sign In</p>
+                        </div>
+                    </>
+
+                }
+                
+            </div>
+        </div>
+    )
+}
+
+export default Landing
