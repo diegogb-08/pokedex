f0c2e4ed91cd05abac31f030be2fca3fadfef13b
diego <d.garciabrisa@gmail.com>
2021-05-07 13:46:43 +0200

    added name to pokeCard

--- frontend/src/components/PokeCard/PokeCard.jsx
+++ frontend/src/components/PokeCard/PokeCard.jsx
@@ -6,6 +6,7 @@ const PokeCard = ({pokemon}) => {
     return (
         <div className="pokeCardComponent">
             <img src={pokemon.sprites.front_default} alt={pokemon.name} className="pokeImg"/>
+            <h3>{pokemon.name}</h3>
         </div>
     )
 }
