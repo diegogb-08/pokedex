1d2464298811b00c591bed44715e0ef9a8405aeb
diego <d.garciabrisa@gmail.com>
2021-05-07 23:11:40 +0200

    trying to avoid the loop in the location

--- frontend/src/components/Location/Location.jsx
+++ frontend/src/components/Location/Location.jsx
@@ -12,23 +12,20 @@ const Location = (props) => {
 
     const [filter, setFilter] = useState({
         offset: 0,
-        limit: 20
+        limit: 10
     })
 
     const [locations, setLocations] = useState([])
-    const [url, setUrl] = useState('')
-    console.log(url)
-    console.log(locations)
+    const [area, setArea] = useState('')
+    console.log("VALUE",area)
+
+    const handleChange = (e) => {
+        setArea(e.target.value)
+    }
 
     useEffect(()=>{
         getLocations()
     })
-
-
-    const handleChange = (value) => {
-        console.log(value)
-        setUrl(value)
-    }
 
 
     const getLocations = async () => {
@@ -64,22 +61,23 @@ const Location = (props) => {
                 <div className="spacer"></div>
                 <div className="spacer"></div>
                 <div className="filterLocation">
-                    <FontAwesomeIcon icon={faArrowAltCircleLeft}  className="arrow" onClick={()=>nextPage(-20)}/>
+                    <FontAwesomeIcon icon={faArrowAltCircleLeft}  className="arrow" onClick={()=>nextPage(-10)}/>
 
                         <select name="locations" onChange={handleChange}>
-                            {
-                                locations?.map(area => {
-                                    console.log(area)
-                                    return (
-                                        <>
-                                            <option value={area.url}>{area.name}</option>
-                                        </>
-                                    )
-                                })
-                            }
+                            <option value="" defaultValue>Select area</option>
+                            <option value={locations[0]?.url}>{locations[0]?.name}</option>
+                            <option value={locations[1]?.url}>{locations[1]?.name}</option>
+                            <option value={locations[2]?.url}>{locations[2]?.name}</option>
+                            <option value={locations[3]?.url}>{locations[3]?.name}</option>
+                            <option value={locations[4]?.url}>{locations[4]?.name}</option>
+                            <option value={locations[5]?.url}>{locations[5]?.name}</option>
+                            <option value={locations[6]?.url}>{locations[6]?.name}</option>
+                            <option value={locations[7]?.url}>{locations[7]?.name}</option>
+                            <option value={locations[8]?.url}>{locations[8]?.name}</option>
+                            <option value={locations[9]?.url}>{locations[9]?.name}</option>
                         </select>
 
-                    <FontAwesomeIcon icon={faArrowAltCircleRight} className="arrow" onClick={()=>nextPage(20)}/>
+                    <FontAwesomeIcon icon={faArrowAltCircleRight} className="arrow" onClick={()=>nextPage(10)}/>
 
                 </div>
 
